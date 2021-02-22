2bc39db1dbeea91ecdebb8f17456acdc7aab08e6
Celina <cbasa@qa.com>
2021-02-22 17:11:51 +0000

    People example solution

---
+++ src/Components/Props/PeopleSolution/User.jsx
@@ -0,0 +1,24 @@
+'use strict';
+
+import Individual from './Individual';
+import Humans from './People.json';
+
+const User = () => {
+
+    return (
+        <>
+            {Humans.map((person) => (
+                <Individual
+                    key={person.id}
+                    name={person.name}
+                    city={person.address.city}
+                    info={person}
+                    website={person.website}
+                />
+            ))}
+        </>
+    );
+
+}
+
+export default User; 
