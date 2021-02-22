90069e54cbd8247061004f3f651eb9cba7fb8394
Celina <cbasa@qa.com>
2021-02-22 14:26:52 +0000

    Made changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+'use strict';
 
+import Intro from "./Components/First";
+import Standard from "./Components/Parents-Child/Standard";
+import Parent from "./Components/Props/Simple/Parent";
+import PropComp from "./Components/Props_Tutorial/PropComp";
+import Second from "./Components/Second";
 
-function App() {
+// Component should start with capital letter (name of function)
+const App = () => {
   return (
     <div className="App">
-      <p>hello</p>
+      {/* <Standard /> */}
+      {/* <Parent /> */}
+      <PropComp />
     </div>
   );
 }
