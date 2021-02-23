3d65ca08043d57b9bf736fd32317805db72d2d6d
Celina <cbasa@qa.com>
2021-02-23 11:59:01 +0000

    Made changes to file

--- src/Components/Props/PeopleSolution/Individual.jsx
+++ src/Components/Props/PeopleSolution/Individual.jsx
@@ -1,9 +1,12 @@
 'use strict';
 
+import PropTypes from 'prop-types';
+
 const Individual = (props) => {
 
     return (
         <>
+            <h3>{props.headerText}</h3>
             <p key={props.id}>Hello {props.name}, i see you're from {props.city}</p>
             <a href={props.website}>Link to their website</a>
         </>
@@ -11,3 +14,12 @@ const Individual = (props) => {
 }
 
 export default Individual;
+
+Individual.defaultProps = {
+    headerText: `if nothing is passed in, use this value`
+}
+
+Individual.propTypes = {
+    name: PropTypes.string.isRequired,
+    id: PropTypes.number
+}
