23405de875aeef229a8aa7fbf14d7e43fe2b4b4e
Celina <cbasa@qa.com>
2021-02-23 11:59:15 +0000

    Added States

---
+++ src/Components/States/ChildComponent.jsx
@@ -0,0 +1,12 @@
+'use strict';
+
+const ChildComponent = ({ count }) => {
+
+    return (
+        <>
+            <h3>I am child component with count: {count} </h3>
+        </>
+    )
+}
+
+export default ChildComponent;
---
+++ src/Components/States/DifferentStates.jsx
@@ -0,0 +1,19 @@
+'use strict';
+
+import { useState } from "react";
+
+const DifferentStates = () => {
+
+    // Create my state
+    const [edit, setEdit] = useState(false);
+
+    return (
+        <>
+            <p>The value of edit is: {edit}</p>
+            {edit ? <p>True</p> : <p>False</p>}
+            <button onClick={() => setEdit(c => !c)}>Update</button>
+        </>
+    )
+}
+
+export default DifferentStates;
